432eb8ccbb2376723287e9df7a473f96cf47a885
cike <1139792278@qq.com>
2015-05-05 10:25:20 +0800

    ok-good

--- KeyBoardTest/gen/com/example/keyboardtest/R.java
+++ KeyBoardTest/gen/com/example/keyboardtest/R.java
@@ -19,9 +19,15 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
+        public static final int key_height=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int sym_keyboard_delete=0x7f020001;
+        public static final int sym_keyboard_left=0x7f020002;
+        public static final int sym_keyboard_right=0x7f020003;
+        public static final int sym_keyboard_shift=0x7f020004;
+        public static final int sym_keyboard_space=0x7f020005;
     }
     public static final class id {
         public static final int action_settings=0x7f090003;
