101edabab933275948c9b8d139ac7043446805cc
cike <1139792278@qq.com>
2015-05-11 17:59:13 +0800

    wechat

---
+++ TestViewPage/src/com/anxiong/fragment/CFragment.java
@@ -0,0 +1,22 @@
+package com.anxiong.fragment;
+
+import com.example.testviewpage.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CFragment extends Fragment {
+	
+	private View view;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		view=inflater.inflate(R.layout.c, null);
+		return view;
+	}
+
+}
